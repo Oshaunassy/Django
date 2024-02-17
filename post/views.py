6806cd5ee314ac518ae34f9f0f7d203fbfce5cd1
oshaunassy <burul_nbc@mail.ru>
2024-02-17 16:19:09 +0600

    homework 1

---
+++ post/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+
+# Create your views here.
+
+def test_view(request):
+    return HttpResponse('Hello, it is my project')
+
+def main_view(reguest):
+    if reguest.method == 'GET':
+        return render(reguest, 'main.html')
+    # elif reguest.method == 'POST':
+
+def current_date(request):
+    now = datetime.now()
+    return HttpResponse(f"Current date and time: {now}")
+
+def goodbye(request):
+    return HttpResponse("Goodbye user!")
